7b3baf6d09e3c6e48c33d25055ec670ea2c7b1c1
Unknown <volzstefan97@googlemail.com>
2018-11-10 10:42:26 +0100

    Changed ContextSession to be a factory
    Allows for multiple Sessions at once/ Not bound to one Engine. Also added Docstrings

--- TUInventory/classes.py
+++ TUInventory/classes.py
@@ -12,80 +12,48 @@ Base = declarative_base()
 engine = sqlalchemy.create_engine('sqlite:///:memory:', echo=True)
 
 class BigInt(sqlalchemy.types.TypeDecorator):
+    """SQLAlchemy datatype for dealing with ints that potentially overflow a basic SQL Integer"""
     impl = sqlalchemy.types.Integer
-
     def process_bind_param(self, value, dialect):
-        """Gets called when writing to db
-        """
+        """Gets called when writing to db"""
         return str(value)
     def process_result_value(self, value, dialect):
-        """Gets called when reading from db
-        """
+        """Gets called when reading from db"""
         return int(value)
 
 class ContextSession():
-    """ContextManager for Session objects
-    """
-    _engine = None
-    _Session = None
+    """Factory for contextmanagers for Session objects"""
+    @classmethod
     def setup(self, engine):
-        self._engine = engine
-        self._Session = orm.sessionmaker(bind=engine)
-    def __enter__(self):
-        self.session = self._Session()
-        return self.session
-    def __exit__(self, exc_type, exc_value, traceback):
-        if exc_value or exc_type or traceback:
-            self.session.rollback()
-            return False
-        else:
-            self.session.commit()
-            self.session.close()
-            return True
+        """"Initialize a ContextSession class
+        Args:
+            engine (sqlalchemy.engine.base.Engine): Engine that's bound to the sessionmaker
+        Example:
+            engine = sqlalchemy.create_engine('sqlite:///:memory:')
+            CSession = ContextSession(engine)
+            with CSession as session:
+                session.add(user1)
+                session.add(user2)
+        """
+        class ContextSession():
+            _engine = engine
+            _Session = orm.sessionmaker(bind=engine)
+            def __enter__(self):
+                self.session = self._Session()
+                return self.session
+            def __exit__(self, exc_type, exc_value, traceback):
+                if exc_value or exc_type or traceback:
+                    self.session.rollback()
+                    return False
+                else:
+                    self.session.commit()
+                    self.session.close()
+                    return True
+        return ContextSession
 
-
-class User(Base):
-    __tablename__ = "users"
-    uid = Column(Integer, primary_key=True)
-    e_mail = Column(String, unique=True)
-    password = Column(LargeBinary)
-    salt = Column(BigInt)
-    name = Column(String)
-    surname = Column(String)
-    location = Column(Integer)
-    phone_nr = Column(String)
-    def __init__(self, e_mail, password, name, surname, location, phone_nr, uid=None, salt=None):
-        self.uid = uid
-        self.e_mail = e_mail.lower()
-        self.salt = salt if salt else randbits(256)
-        self.name = name.lower()
-        self.surname = surname.lower()
-        self.location = location
-        self.phone_nr = self._normalize_phone_nr(phone_nr)
-        self.password = self.hash(password)
-    def hash(self, password):
-        return hashlib.pbkdf2_hmac(hash_name="sha512", 
-            password=password.encode(), 
-            salt=str(int.from_bytes(self.e_mail.encode(), byteorder="big") ^ self.salt).encode(), 
-            iterations=9600)
-    @staticmethod
-    def _normalize_phone_nr(input):
-        re.match(r"[0-9+49]", input)
-        normalized = None
-        return normalized
-
-class Location(Base):
-    __tablename__ = "locations"
-    uid = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
-    
-class Responsibility(Base):
-    __tablename__ = "responsibilities"
-    device_uid = Column(Integer, primary_key=True)
-    user_uid = Column(Integer, primary_key=True)
-    location_uid = Column(Integer, primary_key=True)
-    
 class PhoneNumber():
+    __slots__ = ("raw_string", "match", "country_code", "area_code", "subscriber_number", "extension", "pattern")
+    pattern = r"(((\+\d{1,3})|(0)) ?([1-9]+) )?(\d+ ?)+(-\d+)?"
     def __init__(self, raw_string):
         """Get a Phone Number from raw input string
         Args:
@@ -101,7 +69,6 @@ class PhoneNumber():
         self.area_code = self._extract_area_code()
         self.subscriber_number = self._extract_subscriber_number()
         self.extension = self._extract_extension()
-    pattern = r"(((\+\d{1,3})|(0)) ?([1-9]+) )?(\d+ ?)+(-\d+)?"
     class NoNumberFoundWarning(Warning):
         def __init__(self, raw_string):
             super().__init__(self)
@@ -109,6 +76,7 @@ class PhoneNumber():
             self.args[1] = raw_string
     @staticmethod
     def _whitespacekiller(string):
+        """Remove all non-number characters from a string"""
         return re.sub(r"\D", "", string)
     def _extract_country_code(self):
         country_code = self.match.group(2)
@@ -126,10 +94,54 @@ class PhoneNumber():
         extension = self.match.group(7)
         return self._whitespacekiller(extension)
     def __str__(self):
+        """Build string of the telephone number based on DIN 5008
+        """
         extension = f"-{self.extension}" if self.extension else ""
         return "f+{self.country_code} {self.area_code} {self.subscriber_number}{extension}"
 
-Base.metadata.create_all(bind=engine) # Datenbank initialisiert
+class User(Base):
+    """Represents a user of the application"""
+    __tablename__ = "users"
+    uid = Column(Integer, primary_key=True)
+    e_mail = Column(String, unique=True)
+    password = Column(LargeBinary)
+    salt = Column(BigInt)
+    name = Column(String)
+    surname = Column(String)
+    location = Column(Integer)
+    phone_nr = Column(String)
+    def __init__(self, e_mail, password, name, surname, location, phone_nr, uid=None, salt=None):
+        self.uid = uid
+        self.e_mail = e_mail.lower()
+        self.salt = salt if salt else randbits(256)
+        self.name = name.lower()
+        self.surname = surname.lower()
+        self.location = location
+        self.phone_nr = PhoneNumber(phone_nr)
+        self.password = self.hash(password)
+    def hash(self, password):
+        """Hash a string with pbkdf2
+        The salt is XORd with the e_mail to get the final salt
+        """
+        return hashlib.pbkdf2_hmac(hash_name="sha512", 
+            password=password.encode(), 
+            salt=str(int.from_bytes(self.e_mail.encode(), byteorder="big") ^ self.salt).encode(), 
+            iterations=9600)
+
+class Location(Base):
+    """Represents a physical location where a Device or User may be located"""
+    __tablename__ = "locations"
+    uid = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    
+class Responsibility(Base):
+    """Represents a responsibility a User has for a Device"""
+    __tablename__ = "responsibilities"
+    device_uid = Column(Integer, primary_key=True)
+    user_uid = Column(Integer, primary_key=True)
+    location_uid = Column(Integer, primary_key=True)
+    
+Base.metadata.create_all(bind=engine) # Database initialized
 
 user1 = User(e_mail="Schokoladenkönig@googlemail.com", password="12345ibimsdaspasswort", name="Schoko", surname="König", location=5, phone_nr="123456")
 user2 = User(e_mail="Oladenkönig@googlemail.com", password="12msdassort", name="Schoko", surname="Künig", location=45, phone_nr="654321")
