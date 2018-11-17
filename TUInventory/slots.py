14a3305234100cd34d28e8e961d1649577ee9d55
Unknown <volzstefan97@googlemail.com>
2018-11-17 22:25:21 +0100

    File to hold the logic that's assigned to the UI signals

---
+++ TUInventory/slots.py
@@ -0,0 +1,112 @@
+import threading
+from time import sleep, time
+
+import sqlalchemy
+
+import classes
+
+Session = sqlalchemy.orm.sessionmaker(bind=classes.engine)
+CSession = classes.ContextSession.setup(classes.engine)
+
+def save_to_db(instance):
+    """Save instance to it's corresponding table
+    ToDo: Error handling if uid already exists
+    """
+    with CSession() as session:
+        session.add(instance)
+
+def toggle_admin_tools(is_admin):
+    pass # hide and show widgets depending on admin_status
+
+def update_user_dependant(user):
+    pass # update elements that hold user-sensitive data like a name on display
+
+def create_user():
+    e_mail = ""
+    password = ""
+    name = ""
+    surname = ""
+    location = ""
+    phonenumber = ""
+    new_user = classes.User(e_mail, password, name, surname, phonenumber)
+    new_user.location = location
+    save_to_db(new_user)
+    return new_user
+
+def create_admin():
+    """Create a new admin"""
+    new_admin = create_user()
+    new_admin.is_admin = True
+    save_to_db(new_admin)
+    return new_admin
+
+def login():
+    """Log user into application"""
+    e_mail = ""
+    password = ""
+    user_at_gate = classes.User(e_mail, password)
+    try:
+        with CSession() as session:
+            user = session.query(classes.User).filter_by(e_mail == user_at_gate.e_mail).first()
+    except Exception as e: #user not found exception
+        pass # show error message
+    if user_at_gate.password == user.password:
+        toggle_admin_tools(user.is_admin)
+        update_user_dependant(user)
+        return user
+    else:
+        return None
+
+def logout():
+    """Log user out of application"""
+    pass
+
+class Timeout(threading.Thread):
+    def __init__(self, timeout, resolution, function, args=[]):
+        """Timer that runs in background and executes a function if it's not refreshed
+        Args:
+            function: function that is executed once time runs out
+            resolution: time in seconds that between timeout checks
+            timeout: time in seconds after which the timeout executes the function
+            args: arguments for function
+        """
+        super().__init__()
+        self.timeout = timeout
+        self.timer = 0
+        self.resolution = resolution
+        self.function = function
+        self.args = args
+        self.lock = threading.Lock()
+        self.timed_out = False
+
+    def reset(self):
+        """Reset the internal timer"""
+        self.timer = 0
+
+    def run(self):
+        t1 = time()
+        if self.timer > self.timeout:
+            self.timed_out = True
+            self.function(*self.args)
+        else:
+            t2 = time()
+            sleep(self.resolution - (t2 - t1))
+            self.timer += self.resolution
+            self.run()
+
+if __name__ == "__main__":
+
+    timeout = Timeout(timeout=10, resolution=2, function=print, args=["timed out"])
+    timeout.start()
+
+    reset_thread = threading.Thread(target=lambda: timeout.reset() if input() else None) # function for testing - reset on input
+    reset_thread.start()
+    t = 0
+    delta_t = 3
+    while not timeout.timed_out:
+        print(f"Not timed out yet - {t:.2f} seconds passed")
+        t += delta_t
+        print(f"internal timer is at {timeout.timer:.2f}s")
+        sleep(delta_t)
+    timeout.join()
+    reset_thread.join()
