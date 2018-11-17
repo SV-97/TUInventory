7ca50228f62982f1be69bab81b333bb91020aed8
Unknown <volzstefan97@googlemail.com>
2018-11-17 22:24:57 +0100

    Added Phonenumber table and relationship and PEP8-ified the code

--- TUInventory/classes.py
+++ TUInventory/classes.py
@@ -10,8 +10,11 @@ orm = sqlalchemy.orm
 
 Base = declarative_base()
 
-engine = sqlalchemy.create_engine("sqlite:///:memory:", echo=False)
-# engine = sqlalchemy.create_engine("sqlite:///test.db", echo=False)
+with open("TUInventory/test.db", "w") as f:
+    f.flush()
+engine = sqlalchemy.create_engine("sqlite:///TUInventory/test.db", echo=False)
+# engine = sqlalchemy.create_engine("sqlite:///:memory:", echo=False)
+
 
 class BigInt(sqlalchemy.types.TypeDecorator):
     """SQLAlchemy datatype for dealing with ints that potentially overflow a basic SQL Integer"""
@@ -19,9 +22,11 @@ class BigInt(sqlalchemy.types.TypeDecorator):
     def process_bind_param(self, value, dialect):
         """Gets called when writing to db"""
         return str(value)
+
     def process_result_value(self, value, dialect):
         """Gets called when reading from db"""
         return int(value)
+
 
 class ContextSession():
     """Factory for contextmanagers for Session objects"""
@@ -33,7 +38,7 @@ class ContextSession():
         Example:
             engine = sqlalchemy.create_engine('sqlite:///:memory:')
             CSession = ContextSession(engine)
-            with CSession as session:
+            with CSession() as session:
                 session.add(user1)
                 session.add(user2)
         """
@@ -53,6 +58,7 @@ class ContextSession():
                     return True
         return ContextSession
 
+
 class Producer(Base):
     """Represents a producer of articles"""
     __tablename__ = "producers"
@@ -62,6 +68,7 @@ class Producer(Base):
     def __init__(self, name, uid=None):
         self.uid = uid
         self.name = name
+
 
 class Article(Base):
     """Represents an article"""
@@ -76,6 +83,7 @@ class Article(Base):
         self.name = name
         self.producer = producer
 
+
 class Device(Base):
     """Represents an article"""
     __tablename__ = "devices"
@@ -89,8 +97,10 @@ class Device(Base):
         self.uid = uid
         self.code = code
 
-class PhoneNumber(): #ToDo: Doesn't inherit from Base as of now!
+
+class PhoneNumber(Base):
     __tablename__ = "phone_numbers"
+    user_uid = Column(Integer, sqlalchemy.ForeignKey("users.uid"), primary_key=True)
     raw_string = Column(String)
     country_code = Column(String)
     area_code = Column(String)
@@ -112,39 +122,48 @@ class PhoneNumber(): #ToDo: Doesn't inherit from Base as of now!
         self.area_code = self._extract_area_code()
         self.subscriber_number = self._extract_subscriber_number()
         self.extension = self._extract_extension()
+
     class NoNumberFoundWarning(Warning):
         def __init__(self, raw_string):
             super().__init__(self)
             self.args[0] = f"Unable to find phone number in raw_string"
             self.args[1] = raw_string
+
     @staticmethod
     def _whitespacekiller(string):
         """Remove all non-number characters from a string"""
         return re.sub(r"\D", "", string)
+
     def _extract_country_code(self):
         country_code = self.match.group(2)
         if country_code and "+" in country_code:
             return self._whitespacekiller(country_code)
         else:
             return "049"
+
     def _extract_area_code(self):
         area_code = self.match.group(5) if self.match.group(5) else "9321"
         return self._whitespacekiller(area_code)
+
     def _extract_subscriber_number(self):
         subscriber_number = self.match.group(6)
         return self._whitespacekiller(subscriber_number)
+
     def _extract_extension(self):
         extension = self.match.group(7)
         extension = "" if not extension else extension
         return self._whitespacekiller(extension)
+
     def __str__(self):
         """Build string of the telephone number based on DIN 5008
         """
         extension = f"-{self.extension}" if self.extension else ""
         return f"+{self.country_code} {self.area_code} {self.subscriber_number}{extension}"
+
     def __format__(self, format_spec):
         return f"{str(self):{format_spec}}"
-    
+
+
 class Location(Base):
     """Represents a physical location where a Device or User may be located"""
     __tablename__ = "locations"
@@ -156,11 +175,9 @@ class Location(Base):
         self.name = name
         self.uid = uid
 
+
 class User(Base):
-    """Represents a user of the application
-    ToDo:
-        - PhoneNumber relationship(they seem to be working as of now but aren't even in the database(?))
-    """
+    """Represents a user of the application"""
     __tablename__ = "users"
     uid = Column(Integer, primary_key=True)
     e_mail = Column(String, unique=True)
@@ -172,15 +189,17 @@ class User(Base):
     location_uid = Column(Integer, sqlalchemy.ForeignKey("locations.uid"))
     location = orm.relationship("Location", backref=orm.backref("users", uselist=False))
     responsibilities = orm.relationship("Responsibility", backref="user")
-    def __init__(self, e_mail, password, name, surname, phone_nr, uid=None, salt=None):
+    phonenumber = orm.relationship("PhoneNumber", backref="user", uselist=False)
+    def __init__(self, e_mail, password, name=None, surname=None, phonenumber=None, uid=None, salt=None):
         self.uid = uid
         self.e_mail = e_mail.lower()
         self.salt = salt if salt else randbits(256)
         self.name = name.lower()
         self.surname = surname.lower()
-        self.phone_nr = PhoneNumber(phone_nr)
+        self.phonenumber = PhoneNumber(phonenumber)
         self.password = self.hash(password)
         self.is_admin = False
+
     def hash(self, password):
         """Hash a string with pbkdf2
         The salt is XORd with the e_mail to get the final salt
@@ -190,6 +209,7 @@ class User(Base):
             salt=str(int.from_bytes(self.e_mail.encode(), byteorder="big") ^ self.salt).encode(), 
             iterations=9600)
 
+
 class Responsibility(Base):
     """Represents a responsibility a User has for a Device"""
     __tablename__ = "responsibilities"
@@ -197,12 +217,15 @@ class Responsibility(Base):
     user_uid = Column(Integer, sqlalchemy.ForeignKey("users.uid"), primary_key=True)
     location_uid = Column(Integer, sqlalchemy.ForeignKey("locations.uid"), primary_key=True)
     
+
 Base.metadata.create_all(bind=engine) # Database initialized
+
 if __name__ == "__main__":
+    """Tests"""
     location1 = Location("Gebäude 23")
     location2 = Location("Büro")
-    user1 = User(e_mail="Schokoladenkönig@googlemail.com", password="12345ibimsdaspasswort", name="Schoko", surname="König", phone_nr="123456")
-    user2 = User(e_mail="Oladenkönig@googlemail.com", password="12msdassort", name="Schoko", surname="Künig", phone_nr="654321")
+    user1 = User(e_mail="Schokoladenkönig@googlemail.com", password="12345ibimsdaspasswort", name="Schoko", surname="König", phonenumber="123456")
+    user2 = User(e_mail="Ololadenkönig@googlemail.com", password="12msdassort", name="Schoko", surname="Künig", phonenumber="654321")
     user1.location = location1
     user2.location = location2
 
@@ -263,12 +286,13 @@ if __name__ == "__main__":
     locations = session.query()
 
     for user in users:
-        print(f"{user.uid:>5}:   {user.e_mail:>40} | {user.phone_nr:<20} | {user.location.name}")
+        print(f"{user.uid:>5}:   {user.e_mail:>40} | {user.phonenumber:<20} | {user.location.name}")
     print("-"*30)
     print("Known responsibilities for these users are:")
     for user in users:
         for resp in user.responsibilities:
             print(f"{resp.user.name:^15}|{resp.device.code:^15}|{resp.location.name:^15}")
+            print(resp.device.article.name)
 
     session.commit()
     session.close()
