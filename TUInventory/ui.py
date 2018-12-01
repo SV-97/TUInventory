c6e7a7a3c5cd5dad4c2aff76e95ea079aded32eb
Unknown <volzstefan97@googlemail.com>
2018-12-01 02:31:06 +0100

    Connected a few slots, added tree for overview
    ui is no longer the main file

--- TUInventory/ui.py
+++ TUInventory/ui.py
@@ -1,14 +1,16 @@
 import os
 import sys
 import pathlib
-import sqlite3
 import styling
 
 from PyQt5 import uic, QtWidgets
 from PyQt5.QtGui import QColor, QIcon, QPainter, QPen
 from PyQt5.QtCore import Qt
 
-connection_users = sqlite3.connect("tuinventory.db")
+import classes
+import slots
+
+CSession = classes.setup_context_session(classes.engine)
 
 def absolute_path(relative_path):
     path = pathlib.Path(os.path.dirname(__file__))
@@ -21,6 +23,8 @@ class MainDialog(QtWidgets.QDialog):
         path = absolute_path("main_horizontal.ui")
         super().__init__(parent)
         self.ui = uic.loadUi(path, self)
+        self.logged_in_user = None
+        self.set_tree()
         self.ui.b_user_login.clicked.connect(self.b_user_login_click)
         self.ui.b_user_logout.clicked.connect(self.b_user_logout_click)
         self.ui.b_home_1.clicked.connect(self.b_home_1_click)
@@ -121,7 +125,7 @@ class MainDialog(QtWidgets.QDialog):
         self.ui.line_4.hide()
         self.ui.line_5.hide()
 
-    def b_tab_1_click(self):                        # tab 1
+    def b_tab_1_click(self):
         self.ui.stackedWidget.setCurrentIndex(0)
         self.ui.line_1.show()
         self.ui.line_2.hide()
@@ -129,7 +133,7 @@ class MainDialog(QtWidgets.QDialog):
         self.ui.line_4.hide()
         self.ui.line_5.hide()
 
-    def b_tab_2_click(self):                        # tab 2
+    def b_tab_2_click(self):
         self.ui.stackedWidget.setCurrentIndex(1)
         self.ui.line_1.hide()
         self.ui.line_2.show()
@@ -137,7 +141,7 @@ class MainDialog(QtWidgets.QDialog):
         self.ui.line_4.hide()
         self.ui.line_5.hide()
 
-    def b_tab_3_click(self):                        # tab 3
+    def b_tab_3_click(self):
         self.ui.stackedWidget.setCurrentIndex(2)
         self.ui.line_1.hide()
         self.ui.line_2.hide()
@@ -145,7 +149,7 @@ class MainDialog(QtWidgets.QDialog):
         self.ui.line_4.hide()
         self.ui.line_5.hide()
 
-    def b_tab_4_click(self):                        # tab 4
+    def b_tab_4_click(self):
         self.ui.stackedWidget.setCurrentIndex(3)
         self.ui.line_1.hide()
         self.ui.line_2.hide()
@@ -153,7 +157,7 @@ class MainDialog(QtWidgets.QDialog):
         self.ui.line_4.show()
         self.ui.line_5.hide()
 
-    def b_tab_5_click(self):                        # tab 5
+    def b_tab_5_click(self):
         self.ui.stackedWidget.setCurrentIndex(4)
         self.ui.line_1.hide()
         self.ui.line_2.hide()
@@ -161,42 +165,69 @@ class MainDialog(QtWidgets.QDialog):
         self.ui.line_4.hide()
         self.ui.line_5.show()
     #/#    
-    
-    
+
+    def set_tree(self):
+        with CSession() as session:
+            responsibilities = session.query(classes.Responsibility).all()
+            for resp in responsibilities:
+                location = QtWidgets.QTreeWidgetItem([str(resp.location.name)])
+                user = QtWidgets.QTreeWidgetItem([str(resp.user)]) # toDo: Set color if user is the currently logged in one and add this to update_user_dependant
+                device = QtWidgets.QTreeWidgetItem([str(resp.device)])
+                locations = [self.treeWidget.topLevelItem(i) for i in range(self.treeWidget.topLevelItemCount())] # has to be list-comprehension because used multiple times -> docu
+                tree_text = (item.text(0) for item in locations) # can be generator expression because single-use -> docu
+                if str(resp.location.name) not in tree_text: # if there's a device with the name of a location already displayed there's an issue
+                    location.addChild(user)
+                    user.addChild(device)
+                    self.treeWidget.addTopLevelItem(location)
+                else:
+                    for location in locations:
+                        if location.text(0) == str(resp.location):
+                            users = [location.child(i) for i in range(location.childCount())]
+                            if str(resp.user) not in (user.text(0) for user in users):
+                                location.addChild(user)
+                                user.addChild(device)
+                            else:
+                                for user in users:
+                                    devices = (user.child(i) for i in range(location.childCount()))
+                                    if str(resp.device) not in devices:
+                                        user.addChild(device)
+
     def b_user_login_click(self):
-        global dialog_login
-        dialog_login.exec() # show dialog_login as modal dialog => blocks controll of main
-        self.hide()
-        self.ui.log_in_out.setCurrentIndex(0)
+        LoginDialog(self).exec() # show dialog_login as modal dialog => blocks controll of main
+        self.update_user_dependant()
+        print(self.logged_in_user)
 
     def b_user_logout_click(self):
-        self.ui.log_in_out.setCurrentIndex(1)
+        self.logged_in_user = None # may want slots.logout if that does something eventually
+        self.update_user_dependant()
 
+    def update_user_dependant(self):
+        if self.logged_in_user:
+            self.ui.log_in_out.setCurrentIndex(0)
+            self.label.setText(str(self.logged_in_user).title())
+        else:
+            self.ui.log_in_out.setCurrentIndex(1)
+            self.label.setText("")
 
 class LoginDialog(QtWidgets.QDialog):
     
     def __init__(self, parent=None):
         path = absolute_path("login.ui")
         super().__init__(parent)
+        self.parent = parent
         self.ui = uic.loadUi(path, self)
         self.b_login.clicked.connect(self.b_login_click)
+
     def b_login_click(self):
-        connection = connection_users
         username = self.t_username.text()
         password = self.t_password.text()
-        with connection:
-            cursor = connection.cursor()
-            cursor.execute("SELECT user FROM users WHERE username=?", username)
-            user = cursor.fetchall()
-        if user:
-            pass # proceed with log in/check password, close dialog
-        else:
-            pass # show error message
+        self.parent.logged_in_user = slots.login(username, password)
+        self.close()
 
-
-app = QtWidgets.QApplication(sys.argv)
-dialog_main = MainDialog()
-dialog_login = LoginDialog()
-dialog_main.show() # show dialog_main as modeless dialog => return control back immediately
-
-sys.exit(app.exec_())
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    dialog_main = MainDialog()
+    dialog_login = LoginDialog()
+    dialog_main.show() # show dialog_main as modeless dialog => return control back immediately
+    
+    sys.exit(app.exec_())
