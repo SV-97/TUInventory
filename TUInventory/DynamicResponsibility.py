aa387d218221ce5288abe55ef0eabfc00deaa55b
Unknown <volzstefan97@googlemail.com>
2018-12-01 02:28:41 +0100

    Some more testing regarding trees

--- TUInventory/DynamicResponsibility.py
+++ TUInventory/DynamicResponsibility.py
@@ -20,26 +20,24 @@ locations = 10
 users_per_location = 15
 devices = itertools.count()
 data = {f"Location{i}": {f"user{j}": [str(next(devices)) for k in range(random.randint(1,6))] for j in range(users_per_location)} for i in range(locations) }
-
 pp.pprint(data)
+
 class MainDialog(QtWidgets.QDialog):
     def __init__(self, parent=None):
         path = absolute_path("DynamicResponsibility.ui")
         super().__init__(parent)
         self.ui = uic.loadUi(path, self)
+        
         locations = [QtWidgets.QTreeWidgetItem([location]) for location in data]
-        """
-        for location in data:
-            item = QtWidgets.QTreeWidgetItem([location])
-            self.treeWidget.addTopLevelItem(item)
-        """
         for location in locations:
             users = [QtWidgets.QTreeWidgetItem([child]) for child in data[location.text(0)].keys()]
             for user in users:
                 devices = [QtWidgets.QTreeWidgetItem([device]) for device in data[location.text(0)][user.text(0)]]
                 user.addChildren(devices)
             location.addChildren(users)
+        
         self.treeWidget.addTopLevelItems(locations)
+        print(users[1].child(0).text(0))
 
 
 app = QtWidgets.QApplication(sys.argv)
