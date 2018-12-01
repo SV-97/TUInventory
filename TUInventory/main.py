294a94214fa98f35f8ad144ab7f933e6cd6463d0
Unknown <volzstefan97@googlemail.com>
2018-12-01 15:17:30 +0100

    Added videostream

--- TUInventory/main.py
+++ TUInventory/main.py
@@ -5,9 +5,24 @@ from PyQt5.QtGui import QColor, QIcon, QPainter, QPen
 from PyQt5.QtCore import Qt
 
 import ui
+from barcodereader import VideoStream
 
 app = QtWidgets.QApplication(sys.argv)
 dialog_main = ui.MainDialog()
 dialog_login = ui.LoginDialog()
-dialog_main.exec() # show dialog_main as modeless dialog => return control back immediately
+
+videostream = VideoStream()
+# videostream.start()
+
+dialog_main.show() # show dialog_main as modeless dialog => return control back immediately
+
+"""
+if counter:
+    code = Counter(counter).most_common(1)[0]
+    if code[1] > 20:
+        print(code[0])
+        cv2.destroyWindow(window)
+        sleep(5)
+        return"""
+
 sys.exit(app.exec_())
