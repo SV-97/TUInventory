b4a23fb8a3c11a8ea59c7c00f502996ee5608ffc
Unknown <volzstefan97@googlemail.com>
2018-12-08 21:13:26 +0100

    Added methods to create devices, generate and reset passwords

--- TUInventory/slots.py
+++ TUInventory/slots.py
@@ -1,7 +1,10 @@
+from secrets import choice, compare_digest
+from string import ascii_letters, digits
 
 import sqlalchemy
 
 import classes
+import keys
 
 # Session = sqlalchemy.orm.sessionmaker(bind=classes.engine)
 CSession = classes.setup_context_session(classes.engine)
@@ -13,27 +16,17 @@ def save_to_db(instance):
     with CSession() as session:
         session.add(instance)
 
-def toggle_admin_tools(is_admin):
-    pass # hide and show widgets depending on admin_status
-
 def update_user_dependant(user):
-    pass 
+    pass
 
-def create_user():
-    e_mail = ""
-    password = ""
-    name = ""
-    surname = ""
-    location = ""
-    phonenumber = ""
+def create_user(e_mail, password, name, surname, location, phonenumber):
     new_user = classes.User(e_mail, password, name, surname, phonenumber)
     new_user.location = location
     save_to_db(new_user)
     return new_user
 
-def create_admin():
+def create_admin(new_admin):
     """Create a new admin"""
-    new_admin = create_user()
     new_admin.is_admin = True
     save_to_db(new_admin)
     return new_admin
@@ -60,6 +53,45 @@ def logout():
     """Log user out of application"""
     pass 
 
+def create_device(article):
+    new_device = classes.Device()
+    new_device.article = article
+    save_to_db(new_device)
+    return new_device
+
+def create_location():
+    pass
+
+def create_producer():
+    pass
+
+def generate_password(len_=15):
+    """Generate an human readable password of given length"""
+    alphabet = ascii_letters
+    without = list("O0Il")
+    pw_chars = alphabet + digits + "!,;.-_+-*()[]{}$%=?€#'~ß§&"
+    pw_chars = "".join((letter for letter in pw_chars if letter not in without))
+    print(len(pw_chars))
+    pw = "".join((choice(pw_chars) for i in range(len_)))
+    return pw
+
+def reset_password(user):
+    user.salt = user.new_salt()
+    password = generate_password()
+    user.hash(password)
+    return password
+
+def reset_admin_password(user, path_public, path_private):
+    #catch error if invalid key or no public key
+    cipher, decipher = keys.read_keys(path_public, path_private)
+    text = b"True"
+    ciphertext = cipher.encrypt(text)
+    if compare_digest(decipher.decrypt(ciphertext), text):
+        keys.generate_key(path_public, path_private)
+        new_password = reset_password(user)
+        return new_password
+    else:
+        return None
 
 if __name__ == "__main__":
     user = login("schokoladenkönig@googlemail.com", "12345ibimsdaspasswort")
