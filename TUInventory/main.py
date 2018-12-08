b549f78a8a8b88b5ceacba7427632710fe2bf937
Unknown <volzstefan97@googlemail.com>
2018-12-08 21:14:25 +0100

    added if __name__=="__main__"

--- TUInventory/main.py
+++ TUInventory/main.py
@@ -7,22 +7,23 @@ from PyQt5.QtCore import Qt
 import ui
 from barcodereader import VideoStream
 
-app = QtWidgets.QApplication(sys.argv)
-dialog_main = ui.MainDialog()
-dialog_login = ui.LoginDialog()
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    dialog_main = ui.MainDialog()
+    dialog_login = ui.LoginDialog()
 
-videostream = VideoStream()
-# videostream.start()
+    videostream = VideoStream()
+    # videostream.start()
 
-dialog_main.show() # show dialog_main as modeless dialog => return control back immediately
+    dialog_main.show() # show dialog_main as modeless dialog => return control back immediately
 
-"""
-if counter:
-    code = Counter(counter).most_common(1)[0]
-    if code[1] > 20:
-        print(code[0])
-        cv2.destroyWindow(window)
-        sleep(5)
-        return"""
+    """
+    if counter:
+        code = Counter(counter).most_common(1)[0]
+        if code[1] > 20:
+            print(code[0])
+            cv2.destroyWindow(window)
+            sleep(5)
+            return"""
 
-sys.exit(app.exec_())
+    sys.exit(app.exec_())
