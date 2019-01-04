d7f7d17e0b6318ea17c2e7285c78dfbf107eba16
SV-97 <volzstefan97@googlemail.com>
2019-01-04 14:34:14 +0100

    fixed a typo - was wrong number

--- TUInventory/Documentation/Beispielprogramme/In_DB_schreiben.py
+++ TUInventory/Documentation/Beispielprogramme/In_DB_schreiben.py
@@ -5,4 +5,4 @@ CSession = setup_context_session(engine)
 # ... Instanziieren der Klassen zu user1 und user2 sowie location1
 with CSession() as session:
     session.add(user1)
-    session.add_all([user1, location1])
+    session.add_all([user2, location1])
