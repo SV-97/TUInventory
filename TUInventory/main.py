2a9c70bfd1b7955118bd7943a0a537ebba6d0d6a
Unknown <volzstefan97@googlemail.com>
2019-02-18 13:08:52 +0100

    Beautified import

--- TUInventory/main.py
+++ TUInventory/main.py
@@ -5,7 +5,7 @@ import sys
 from threading import Lock, Thread
 from time import sleep
 
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication
 
 from barcodereader import LazyVideoStream, VideoStream
 from classes import VideoStreamUISync
@@ -15,7 +15,7 @@ from utils import absolute_path
 
 
 def main():
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     dialog_main = ui.MainDialog()
     dialog_main.show()
     
