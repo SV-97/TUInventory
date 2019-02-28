b9409bb98b4d4eb88cce0c2fb6aea92aea6738a8
SV-97 <volzstefan97@googlemail.com>
2019-02-28 11:51:29 +0100

    added path validation utlities - need to move tests to unittest

--- TUInventory/utils.py
+++ TUInventory/utils.py
@@ -3,12 +3,15 @@
 import pathlib
 import os
 from queue import Queue
+import re
 from threading import Thread
+
 
 def absolute_path(relative_path):
     """Convert a path relative to the sourcefile to an absolute one"""
     path = pathlib.Path(os.path.dirname(__file__))
     return path / relative_path
+
 
 class _ParallelPrint(Thread):
     """Provides a threadsafe print, instantiation below"""
@@ -36,3 +39,33 @@ class _ParallelPrint(Thread):
 _ParallelPrint = _ParallelPrint()
 _ParallelPrint.start()
 parallel_print = _ParallelPrint.print_.put
+
+
+def validate_filename(path):
+    """Validate if file already exists"""
+    try:
+        open(path, "x")
+    except FileExistsError as e:
+        return False
+    else:
+        os.remove(path)
+        return True
+
+
+def normalize_filename(string):
+    """Remove all non ASCII letters and digits from a string and 
+    replace whitespaces with underscores keeping dots untouched"""
+    segs = string.split(".")
+    for i, seg in enumerate(segs):
+        segs[i] = re.sub(r"[\W][^\s]", "", seg, flags=re.ASCII)
+        segs[i] = re.sub(r"[\s]", "_", segs[i])
+    return ".".join(segs)
+    
+
+if __name__ == "__main__":
+    assert normalize_filename("123abd.edg/(&(&(%§!14    g\n1gj2141.pdf") == "123abd.edg4___gj2141.pdf"
+
+    assert validate_filename("abc") == True
+    with open("abc", "w") as f:
+        assert validate_filename("abc") == False
+    os.remove("abc")
