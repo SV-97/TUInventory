b78d5cbc76deb22d531f70b3b2f9445c7849233f
YK <y.koehler715@gmail.com>
2019-03-01 12:27:15 +0100

    Created pages for device, article and producer creation

--- TUInventory/main.py
+++ TUInventory/main.py
@@ -2,7 +2,7 @@
 __version__ = "0.1.0"
 
 import logging
-import sys                                                         # --
+import sys
 from threading import Lock, Thread
 from time import sleep
 
