fa9753142e19f7bb9ed21a7c14a314ec7129df9d
Stefan Volz <volzstefan97@googlemail.com>
2019-03-01 21:15:03 +0100

    fixed typo

--- TUInventory/Documentation/Beispielprogramme/regex.py
+++ TUInventory/Documentation/Beispielprogramme/regex.py
@@ -1,5 +1,5 @@
 >>> import re
->>> pattern = re.compile(r"(((\+\d{1,3})|(0)) ?([1-9]+) )?(\d+ ?)+(-\d+)?", re.DEBUG)
+>>> pattern = re.compile(r"(((\+\d{1,3})|(0)) ?([1-9]+) )?((\d+ ?)+)(-\d+)?", re.DEBUG)
 MAX_REPEAT 0 1
   SUBPATTERN 1 0 0
     SUBPATTERN 2 0 0
@@ -19,15 +19,16 @@ MAX_REPEAT 0 1
         IN
           RANGE (49, 57)
     LITERAL 32
-MAX_REPEAT 1 MAXREPEAT
-  SUBPATTERN 6 0 0
-    MAX_REPEAT 1 MAXREPEAT
-      IN
-        CATEGORY CATEGORY_DIGIT
-    MAX_REPEAT 0 1
-      LITERAL 32
+SUBPATTERN 6 0 0
+  MAX_REPEAT 1 MAXREPEAT
+    SUBPATTERN 7 0 0
+      MAX_REPEAT 1 MAXREPEAT
+        IN
+          CATEGORY CATEGORY_DIGIT
+      MAX_REPEAT 0 1
+        LITERAL 32
 MAX_REPEAT 0 1
-  SUBPATTERN 7 0 0
+  SUBPATTERN 8 0 0
     LITERAL 45
     MAX_REPEAT 1 MAXREPEAT
       IN
