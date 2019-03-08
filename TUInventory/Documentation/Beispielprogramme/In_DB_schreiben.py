f60b2a40ebdb3c0ea333470bcd694b0bd69d09b6
Stefan Volz <volzstefan97@googlemail.com>
2019-03-08 16:27:56 +0100

    Fixed typos, reworked graphics

--- TUInventory/Documentation/Beispielprogramme/In_DB_schreiben.py
+++ TUInventory/Documentation/Beispielprogramme/In_DB_schreiben.py
@@ -2,7 +2,7 @@ from classes import engine, setup_context_session
 ... # Ausserdem werden benoetigte Klassen etc. eingebunden
 
 CSession = setup_context_session(engine)
-... #Instanziieren der Klassen zu user1 und user2 sowie location1
+... # Instanziieren der Klassen zu user1 und user2 sowie location1
 with CSession() as session:
     session.add(user1)
     session.add_all([user2, location1])
