5225d8c3e54443162389c7deb85e81eff02845fe
Stefan Volz <volzstefan97@googlemail.com>
2019-03-08 19:45:16 +0100

    May have broken something by leaving this function call in - it's gone

--- TUInventory/slots.py
+++ TUInventory/slots.py
@@ -60,7 +60,6 @@ def login(e_mail, password):
             user = session.query(classes.User).filter_by(e_mail=e_mail).first()
             user_at_gate = classes.User(e_mail, password, salt=user.salt)
             if compare_digest(user_at_gate.password, user.password):
-                update_user_dependant(user)
                 session.expunge(user)
                 logger.info(f"Successfully logged in as {user.uid}")
                 return user
